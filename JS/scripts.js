6c512c3bb142b24ed26d88fca454f12f0fd374bf
Harshin <harshinmehta@gmail.com>
2023-03-03 20:03:01 -0500

    added some page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,128 +1,14 @@
-var options = {
-    shouldSort: true,
-    threshold: 0.4,
-    maxPatternLength: 32,
-    keys: [{
-      name: 'iata',
-      weight: 0.5
-    },
-    {
-      name: 'name',
-      weight: 0.3
-    },
-    {
-      name: 'city',
-      weight: 0.2
-    }]
-};
+function searchFlights() {
+    // Get form elements
+    var tripType = document.querySelector('input[name="radio"]:checked').value;
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    var depart = document.getElementById("depart").value;
+    var returnDate = document.getElementById("return").value;
+    var travellers = document.getElementById("traveller").value;
+  
+    // Redirect to results page with query params
+    var url = "searchFlightsResult.html" + "?tripType=" + tripType + "&from=" + from + "&to=" + to + "&depart=" + depart + "&return=" + returnDate + "&traveller=" + travellers;
+    window.location.href = url;
+  }
 
-var fuse = new fuse(airports, options);
-
-$('.autocomplete').each(function() {
-    var ac = $(this);
-
-    ac.on('click', function(e) {
-        e.stopPropagation();
-    })
-    .on('focus keyup', search)
-    .on('keydown', onKeyDown);
-
-    var wrap = $('<div>')
-        .addClass('autocomplete-wrapper')
-        .insertBefore(ac)
-        .append(ac);
-
-    var list = $('<div>')
-        .addClass('autocomplete-results')
-        .on('click', '.autocomplete-result', function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            selectIndex($(this).data('index'), ac);
-        })
-        .appendTo(wrap);
-});
-
-$(document)
-.on('mouseover', '.autocomplete-result', function(e) {
-    var index = parseInt($(this).data('index'), 10);
-    if (!isNaN(index)) {
-        $(this).attr('data-highlight', index);
-    }
-})
-.on('click', clearResults);
-
-function clearResults() {
-    results = [];
-    numResults = 0;
-    $('.autocomplete-results').empty();
-}
-
-function selectIndex(index, autoinput) {
-    if (results.length >= index + 1) {
-    autoinput.val(results[index].iata);
-    clearResults();
-    }  
-}
-
-var results = [];
-var numResults = 0;
-var selectedIndex = -1;
-
-function search(e) {
-    if (e.which === 38 || e.which === 13 || e.which === 40) {
-        return;
-    }
-    var ac = $(e.target);
-    var list = ac.next();
-    if (ac.val().length > 0) {
-        results = _.take(fuse.search(ac.val()), 7);
-        numResults = results.length;
-
-        var divs = results.map(function(r, i) {
-            return '<div class="autocomplete-result" data-index="'+ i +'">'
-                + '<div><b>'+ r.iata +'</b> - '+ r.name +'</div>'
-                + '<div class="autocomplete-location">'+ r.city +', '+ r.country +'</div>'
-                + '</div>';
-        });
-
-        selectedIndex = -1;
-        list.html(divs.join(''))
-            .attr('data-highlight', selectedIndex);
-
-    } else {
-        numResults = 0;
-        list.empty();
-    }
-}
-
-function onKeyDown(e) {
-    var ac = $(e.currentTarget);
-    var list = ac.next();
-    switch(e.which) {
-        case 38: // up
-            selectedIndex--;
-            if (selectedIndex <= -1) {
-                selectedIndex = -1;
-            }
-            list.attr('data-highlight', selectedIndex);
-        break;
-        case 13: // enter
-        selectIndex(selectedIndex, ac);
-        break;
-        case 9: // enter
-            selectIndex(selectedIndex, ac);
-            e.stopPropagation();
-        return;
-        case 40: // down
-            selectedIndex++;
-            if (selectedIndex >= numResults) {
-                selectedIndex = numResults-1;
-            }
-            list.attr('data-highlight', selectedIndex);
-        break;
-
-        default: return; // exit this handler for other keys
-    }
-    e.stopPropagation();
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-}
---
+++ JS/searchFlightResultsScripts.js
@@ -0,0 +1,38 @@
+function getQueryParam(name) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+
+  // Retrieve query parameters
+  var tripType = getQueryParam("tripType");
+  var from = getQueryParam("from");
+  var to = getQueryParam("to");
+  var depart = getQueryParam("depart");
+  var returnDate = getQueryParam("return");
+  var travellers = getQueryParam("traveller");
+  
+
+  // Display the results
+  document.getElementById("trip-type").textContent = tripType;
+  document.getElementById("from").textContent = from;
+  document.getElementById("to").textContent = to;
+  document.getElementById("depart").textContent = depart;
+  document.getElementById("return").textContent = returnDate;
+  document.getElementById("traveller").textContent = travellers;
+  var ff = from.substr(from.length-3);
+  var tt = to.substr(to.length-3);
+  var urlFetch = 'https://timetable-lookup.p.rapidapi.com/TimeTable/' + ff +'/' + tt + '/20230306/';
+  
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0bd26bb807msha76949a411f105cp139f54jsnb740571f00f9',
+		'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
+	}
+};
+
+fetch(urlFetch, options)
+    .then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
